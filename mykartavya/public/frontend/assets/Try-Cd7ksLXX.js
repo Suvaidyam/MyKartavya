12e61484c2521e14949c592074b480fc87d6ef1e
Ajamat <ajamat.suvaidyam@gmail.com>
2025-02-21 10:15:06 +0530

    add frontend

--- mykartavya/public/frontend/assets/Try-Cd7ksLXX.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,y as t,m as c}from"./index-BjTP7bd0.js";const n={};function r(o,s){return c(),t("div",null," testing ")}const a=e(n,[["render",r]]);export{a as default};
---
+++ mykartavya/public/frontend/assets/mykartavya-logo (1)-BnNVpQoW.js
@@ -0,0 +1,1 @@
+const a="/assets/mykartavya/frontend/assets/mykartavya-logo%20(1)-bFJrvMz-.png";export{a as _};
