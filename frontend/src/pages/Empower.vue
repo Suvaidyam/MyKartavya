ccd835ddd933fe9a830546078471b938cf5a459c
Ajamat <ajamat.suvaidyam@gmail.com>
2025-02-23 14:56:58 +0530

    changes builder and epower pages

--- frontend/src/pages/Empower.vue
+++ frontend/src/pages/Empower.vue
@@ -12,7 +12,7 @@
                 <img src="https://s3-alpha-sig.figma.com/img/5fe6/fe04/058446e1c3ea035d2efd21a222ccb475?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=P-8K1XloauUtT7UVndu4L199Efq0NENaBmmYHpVZpgONi2Uxpbz6sP03gkJBPdo4LkhBZ1fbOgLHSF5Eq3RNvLCuSsdvhVTynPV2e7NlZ9TZqQFww3WueaAI-fXncdfG4xbhgvymccrF-Z0mLImqTne-4fbkk7I4tpHnChzjvmrENAMzsmMxFnhphfuatDa7~5RIlmLt84tmjiz2IBgoSZ3ZhV4WoGL7DQWE6OvV3uJ7JjoFHdUr9mNkWYKbcOJ~q0ehkwRR8XCYap4lG-BdlaOmQdG6ro1QlcixF~YEzhnDfvP1M4Gk00B~4lHnip9Hbzqkwml0NoehBoFPs-zkRQ__"
                     class="w-full h-[456px] object-cover" />
             </div>
-            <div class="absolute left-8 top-48 bg-white shadow-lg rounded-xl p-4 md:p-6 z-10 w-[448px]">
+            <!-- <div class="absolute left-8 top-48 bg-white shadow-lg rounded-xl p-4 md:p-6 z-10 w-[448px]">
                 <h3 class="text-lg font-semibold">Empower the needy</h3>
                 <span class="text-red-500 text-sm">Online</span>
                 <div class="flex items-center mt-2 text-gray-600 text-sm">
@@ -31,7 +31,44 @@
                     <div class="bg-green-500 h-2.5 rounded-full" style="width: 60%"></div>
                 </div>
                 <p class="text-sm text-gray-600 mt-2">60% completed</p>
-            </div>
+            </div> -->
+              <div class="absolute left-8 top-48  max-w-sm w-[448px] h-[312px] bg-white shadow-lg rounded-lg p-4 border border-gray-200 flex flex-col gap-4 justify-center">
+                <div class="border-b pb-2">
+                    <div class="text-gray-500 text-caption font-normal ">My Karma / Kindness & Volunteering / Empower the needy
+                    </div>
+                    <h2 class="text-heading3 font-normal font-poppins mt-1">Empower the needy</h2>
+                    <span class="text-secondary font-medium text-caption">Online</span>
+                </div>
+    
+                <div class="flex items-center    " style="color: #666666;">
+                    <i class="far fa-calendar-alt mr-1 h-5 w-5"></i>
+
+                <span class="flex items-center text-bodyh2 font-normal mr-4" style="color: #0B0B0B;">
+                    01 Oct, 2024 - 30 Nov, 2024
+                </span>
+                </div>
+                <div class="flex items-center  text-gray-600 text-bodyh2 font-normal justify-between border-b pb-2">
+                <span class="flex items-center  justify-center" style="color: #0B0B0B;">
+                    <i class="far fa-clock  h-5 w-5" style="color: #666666;"></i>
+                    50 hr
+                </span>
+                <span class="flex items-center  gap-2 justify-center" style="color: #0B0B0B;">
+                    <i class="fa fa-database text-secondary"></i>
+                    <span class="text-bodyh2 font-normal">  40 Points</span>
+                </span>
+                </div>
+                <div class="flex  space-x-2 border-b pb-2">
+                <img src="https://s3-alpha-sig.figma.com/img/898b/9a5c/5eac9b190fb13207adabb3086b70731b?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=k0HgHMabVhEH5zXQfC7Y-T3VRTGLkbVh3CF-7U3dLsdJKHJn7LY~3g9fVJt6~MzCFuUan1rOU2ScbHhIcSVzRW326B-RGG3SjtQdGg0FR4L6Ib8VHyVQlRnXtA025TCVDifmrnVqQBJCTDxZiWdT2V8hGl8PGBpps1vlIWariZI7Dt2zRrxnHRYNwMUmTbGJS-Dt3egr13OP5smtgglO2PTc6oVst4kc2wykva2uIhlrpdQ~xug9VtkmweOtDhz4k10Db65Oc7UqmnlWhy4xNMSDmjk2zjKfTcjsidzI36OwHf-MHhwjz-weJyChhP5CTRiHn3WASMcQqlp35DsgDA__" class="w-10 h-10" alt="Goal 10" />
+                <img src="https://s3-alpha-sig.figma.com/img/77ec/4184/a463a59fc378c1ee77d824caf08528b3?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=j90JYppUcg2cLQ1-BOIm50A5DQ0mJ~82F7-ukZPIPc7CbhyAPySKPqfz~xshvTL2sSPZXiKq6zqifAp94u8GazYKe6Gz2VO1QjoD5EjW7kHNTsLuw1vM7B0we8dv5mUbFgSK9fJ8-D231cnJuBegTC7yzmw-6Ju-5PUkWjkv7RV6MbxTsVyIsgxxKlaTRid21s1LzA6Jgg4fZrSLvzfpbIbWwwateY3jDQM6DEZ7qS9D3JxkXoZ0RkGDVxcZBCiKADeY9GOQnsjR~RmeiVHFsllKCL2c-0fkcSYz9Wv6UhsMXUKiQ6bxiA1EFWnnuyewI1V4rzd-6q8ly~PwfM3Y0Q__" class="w-10 h-10" alt="Goal 11" />
+                </div>
+    
+    <div class="">
+      <div class="w-full bg-gray-200 rounded-full h-[5px]">
+        <div class="bg-green-500 h-[5px] rounded-full" style="width: 60%"></div>
+      </div>
+      <p class=" text-caption font-normal mt-1" style="color: #0B0B0B;">60% completed</p>
+    </div>
+  </div>
         </section>
         <!-- banner section image end -->
         <!-- main section start  -->
@@ -40,8 +77,8 @@
                 <!-- Main Content -->
                 <div class=" grid gap-6 lg:grid-cols-3">
                     <!-- Left Section -->
-                    <div class="lg:col-span-2  ">
-                        <p class="text-gray-700 mb-4">
+                    <div class="lg:col-span-2   ">
+                        <p class=" mb-4 text-bodyh2 font-normal" style="color: #666666;">
                             <span class="text-[40px] text-black">T</span>his Women’s Entrepreneurship Day, let’s empower
                             underserved women running small businesses or working in our communities by sharing positive
                             reviews of their businesses on our social media platforms. By highlighting their names and
@@ -52,8 +89,8 @@
                             navigate their entrepreneurial journeys. Together, we can create a supportive network that
                             uplifts and inspires!
                         </p>
-                        <h2 class="text-heading4 font-medium mt-6">Volunteer Role</h2>
-                        <p class="text-gray-700 mt-2">
+                        <h2 class="text-heading4 font-medium mt-6" style="color: #0B0B0B;">Volunteer Role</h2>
+                        <p class=" mt-2 text-bodyh2 font-normal" style="color: #666666;">
                             Volunteers are encouraged to identify a small business or an underserved woman in their
                             community and capture a photo or video showcasing her work. Along with the visual, write a
                             positive review that highlights her efforts and talents on your social media platforms. This
@@ -65,21 +102,23 @@
                             uplift and empower these remarkable women, helping them shine in their communities!
                         </p>
                         <div class="mt-4">
-                            <p class="font-semibold " style="word-spacing: 1%;">Share your experience along with
+                            <p class="text-bodyh2 font-normal font-poppins " style="  word-spacing: 28px; letter-spacing: 0.25%; color: #666666;">Share your experience along with
                                 pictures on
-                                your social media handles and tag us on social media using @MyKaratvaya @NASSCOMfdn</p>
+                                your social media handles and tag us on social media using <span class="text-secondary"> @MyKaratvaya @NASSCOMfdn</span></p>
                             <!-- <p class="text-gray-600">@MyKartavya @NASSCOMfdn</p> -->
-                            <p>Use hashtag #MyKartavya #DoGoodFeelGood #LittleActMatters
-                                #womenempowerment#letsgrowtogether
+                            <p class="mt-2 text-bodyh2 font-normal font-poppins" style="color: #666666;">Use hashtag #MyKartavya #DoGoodFeelGood #LittleActMatters #womenempowerment#letsgrowtogether
                             </p>
                         </div>
-                        <div class="mt-4 flex items-center gap-2">
-                            <span class="text-gray-500">Share activity with others:</span>
-                            <div class="flex gap-3">
-                                <i class="fab fa-facebook text-blue-600"></i>
-                                <i class="fab fa-x-twitter text-blue-400"></i>
-                                <i class="fab fa-linkedin text-blue-700"></i>
-                            </div>
+                       <div class="flex items-center space-x-4 flex-col justify-self-start mt-24">
+                        <span class="text-gray-700 font-medium flex items-center space-x-2">
+                            <Share2 class="w-5 h-5 text-gray-600" />
+                            <span class="text-bodyh1 font-medium font-poppins" style="color: #0B0B0B;">Share activity with others</span>
+                        </span>
+                        <div class="flex space-x-2">
+                            <button v-for="icon in icons" :key="icon.name" :aria-label="icon.name" class="p-2 bg-white rounded-full hover:bg-gray-300">
+                            <component :is="icon.svg" class="w-5 h-5 text-gray-600" />
+                            </button>
+                        </div>
                         </div>
                     </div>
 
@@ -92,7 +131,7 @@
 
                 <!-- Related Opportunities -->
                 <div class=" mt-10">
-                    <h2 class="text-heading4 font-medium">Related Opportunities</h2>
+                    <h2 class="text-heading4 font-medium font-poppins" style="color: #0B0B0B;">Related Opportunities</h2>
                     <div class="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-center ">
                         <div class=" bg-white rounded-lg shadow-lg overflow-hidden">
                             <div class="relative">
@@ -113,16 +152,15 @@
                                 </div>
                             </div>
                             <div class="p-4">
-                                <h3 class="text-bodyh1 font-medium text-gray-900">Empower the needy</h3>
-                                <p class="text-gray-600 text-sm mt-1">
+                                <h3 class="text-bodyh1 font-medium " style="color: #0B0B0B;">Empower the needy</h3>
+                                 <p class=" text-caption font-normal mt-1 font-poppins" style="color: #666666;">
                                     This Women’s Entrepreneurship Day, let’s empower underserved women running small
                                     business...
                                 </p>
-                                <div class="flex items-center text-sm text-gray-500 mt-3">
-                                    <span class="flex items-center"><i class="far fa-calendar-alt mr-1"></i> 01 Oct,
+                                  <div class="flex items-center text-sm text-gray-500 mt-3">
+                                    <span class="flex items-center text-caption font-normal" style="color: #0B0B0B;"><p ><i class="far fa-calendar-alt mr-1" style="color: #666666 !important;"></i></p> 01 Oct,
                                         2024 - 30 Nov, 2024</span>
-                                    <span class="ml-auto flex items-center"><i class="far fa-clock mr-1"></i> 50
-                                        hr</span>
+                                    <p class="ml-auto flex items-center text-caption font-normal" style="color: #0B0B0B;">  <span><i class="far fa-clock mr-1"  style="color: #666666 !important; "></i></span>50 hr</p>
                                 </div>
                                 <div class="flex items-center mt-3">
                                     <div class="flex -space-x-2">
@@ -160,16 +198,15 @@
                                 </div>
                             </div>
                             <div class="p-4">
-                                <h3 class="text-bodyh1 font-medium text-gray-900">Bright Bites</h3>
-                                <p class="text-gray-600 text-sm mt-1">
+                                <h3 class="text-bodyh1 font-medium " style="color: #0B0B0B;">Bright Bites</h3>
+                                <p class=" text-caption font-normal mt-1 font-poppins" style="color: #666666;">
                                     This Women’s Entrepreneurship Day, let’s empower underserved women running small
                                     business...
                                 </p>
-                                <div class="flex items-center text-sm text-gray-500 mt-3">
-                                    <span class="flex items-center"><i class="far fa-calendar-alt mr-1"></i> 01 Oct,
+                                 <div class="flex items-center text-sm text-gray-500 mt-3">
+                                    <span class="flex items-center text-caption font-normal" style="color: #0B0B0B;"><p ><i class="far fa-calendar-alt mr-1" style="color: #666666 !important;"></i></p> 01 Oct,
                                         2024 - 30 Nov, 2024</span>
-                                    <span class="ml-auto flex items-center"><i class="far fa-clock mr-1"></i> 50
-                                        hr</span>
+                                    <p class="ml-auto flex items-center text-caption font-normal" style="color: #0B0B0B;">  <span><i class="far fa-clock mr-1"  style="color: #666666 !important; "></i></span>50 hr</p>
                                 </div>
                                 <div class="flex items-center mt-3">
                                     <div class="flex -space-x-2">
@@ -207,16 +244,15 @@
                                 </div>
                             </div>
                             <div class="p-4">
-                                <h3 class="text-bodyh1 font-medium text-gray-900">Beat the sugar</h3>
-                                <p class="text-gray-600 text-sm mt-1">
+                                <h3 class="text-bodyh1 font-medium " style="color: #0B0B0B;">Beat the sugar</h3>
+                                <p class=" text-caption font-normal mt-1 font-poppins" style="color: #666666;">
                                     This Women’s Entrepreneurship Day, let’s empower underserved women running small
                                     business...
                                 </p>
                                 <div class="flex items-center text-sm text-gray-500 mt-3">
-                                    <span class="flex items-center"><i class="far fa-calendar-alt mr-1"></i> 01 Oct,
+                                    <span class="flex items-center text-caption font-normal" style="color: #0B0B0B;"><p ><i class="far fa-calendar-alt mr-1" style="color: #666666 !important;"></i></p> 01 Oct,
                                         2024 - 30 Nov, 2024</span>
-                                    <span class="ml-auto flex items-center"><i class="far fa-clock mr-1"></i> 50
-                                        hr</span>
+                                    <p class="ml-auto flex items-center text-caption font-normal" style="color: #0B0B0B;">  <span><i class="far fa-clock mr-1"  style="color: #666666 !important; "></i></span>50 hr</p>
                                 </div>
                                 <div class="flex items-center mt-3">
                                     <div class="flex -space-x-2">
@@ -251,7 +287,15 @@
 <script setup>
 import Navbar from  '../components/Navbar.vue';
 import Stepper from '../components/Stepper.vue';
+import { h } from 'vue';
+import { Facebook, Twitter, Linkedin, MessageCircle,Share2 } from 'lucide-vue-next';
 
+const icons = [
+  { name: 'Facebook', svg: Facebook },
+  { name: 'X', svg: Twitter },
+  { name: 'LinkedIn', svg: Linkedin },
+  { name: 'WhatsApp', svg: MessageCircle }
+];
 
 
 // export default {
@@ -264,4 +308,10 @@ import Stepper from '../components/Stepper.vue';
   transition: background 0.3s ease;
 } */
 /* @import "@fortawesome/fontawesome-free/css/all.min.css"; */
+</style>
+<style scoped>
+/* Ensures icons have smooth hover transition */
+button {
+  transition: background 0.2s ease-in-out;
+}
 </style>
