bb214f2744197ee1c038516e6ac610688a7f3d0b
vivek620007 <vivekkumar6200168007@gmail.com>
2025-03-04 13:40:24 +0530

    change the validation

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -101,15 +101,9 @@ const routes = [
     meta: { requiresAuth: false }
   },
   {
-<<<<<<< HEAD
     name: 'AllActivity',
-    path: '/all-activity',
-    component: () => import('@/pages/View/Featuredacts.vue'),
-=======
-  name: 'AllActivity',
-  path: '/all-activity/:name',
-  component: () => import('@/pages/View/AllActivity.vue'),
->>>>>>> 45f55427278ea88a9e764e5931a7df656a0c2033
+    path: '/all-activity/:name',
+    component: () => import('@/pages/View/AllActivity.vue'),
   }
 ]
 
