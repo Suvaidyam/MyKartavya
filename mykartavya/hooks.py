48175872871b759284c74143f5478b6b2528ef76
Ajamat <ajamat.suvaidyam@gmail.com>
2025-03-08 13:57:03 +0530

    change builder

--- mykartavya/hooks.py
+++ mykartavya/hooks.py
@@ -15,15 +15,15 @@ required_apps = [
 fixtures=[
     # 'Role',
     # 'Role Profile',
-    # 'Builder Page',
+    'Builder Page',
     # 'SDG',
     # 'Skills',
-    'Property Setter',
+    # 'Property Setter',
     # 'SVADatatable Configuration',
     # "Workflow", "Workflow State", "Workflow Action",
     # "State",
     # "District"
-    # "Builder Client Script",
+    "Builder Client Script",
     # "Website Settings"
 ]
 
