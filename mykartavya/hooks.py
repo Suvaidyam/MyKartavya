a301f3b698237896f44f45ddcf2bcf392e0aa437
Suvaidyam <suv@suv-biitu.lan>
2025-03-12 18:06:35 +0530

    export fixtures

--- mykartavya/hooks.py
+++ mykartavya/hooks.py
@@ -15,7 +15,8 @@ required_apps = [
 fixtures=[
     # 'Role',
     # 'Role Profile',
-    'Builder Page',
+    # 'Builder Page',
+    'Custom HTML Block',
     # 'SDG',
     # 'Skills',
     # 'Property Setter',
@@ -23,7 +24,7 @@ fixtures=[
     # "Workflow", "Workflow State", "Workflow Action",
     # "State",
     # "District"
-    "Builder Client Script",
+    # "Builder Client Script",
     # "Website Settings"
 ]
 
