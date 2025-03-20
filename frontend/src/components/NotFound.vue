acc6d0120b05fb713deb1e79cc3b21d0d50c6b0f
rsuvaidyam <rkrahul00011@gmail.com>
2025-03-20 12:14:38 +0530

    add image

--- frontend/src/components/NotFound.vue
+++ frontend/src/components/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-full h-full flex flex-col justify-center items-center">
-        <img src="../assets/empty.jpg" alt="">
+        <img src="../assets/empty.png" alt="">
         <p class="text-2xl text-gray-600">{{props.message}}</p>
     </div>
 </template>
