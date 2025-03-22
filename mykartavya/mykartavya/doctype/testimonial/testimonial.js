4a527e940f90b978af7e2803c5bf38f51108398e
vishsuvaidyam <vish.suvaidyam@gmail.com>
2025-03-22 15:13:38 +0530

    notification add

--- mykartavya/mykartavya/doctype/testimonial/testimonial.js
+++ mykartavya/mykartavya/doctype/testimonial/testimonial.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, Aniket Singh and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Corporate Partners", {
+// frappe.ui.form.on("Testimonial", {
 // 	refresh(frm) {
 
 // 	},
--- mykartavya/mykartavya/doctype/testimonial/testimonial.py
+++ mykartavya/mykartavya/doctype/testimonial/testimonial.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class CorporatePartners(Document):
+class Testimonial(Document):
 	pass
