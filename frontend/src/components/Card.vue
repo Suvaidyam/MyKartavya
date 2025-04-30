7ceee31980c147593b16728d1797ca8229bf2e22
vishsuvaidyam <vish.suvaidyam@gmail.com>
2025-04-30 11:55:08 +0530

    card inside checnge

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -2,12 +2,12 @@
     <div v-if="type != 'group'" class="card bg-white overflow-hidden">
         <router-link :to="dynamicLinks" class="block">
             <div class="w-full flex flex-col justify-between aspect-[16/9] rounded-lg relative overflow-hidden">
-                <img :src="item.activity_image || 'https://res.cloudinary.com/dyt5jqnax/image/upload/v1742968038/mykartavya-logo_jptv31.png'"
+                <img  :src="item?.activity_image || 'https://res.cloudinary.com/dyt5jqnax/image/upload/v1742968038/mykartavya-logo_jptv31.png'"
                     alt=""
                     class="h-full w-full object-cover rounded-lg transition-transform duration-300 hover:scale-105">
                 <div
                     class="bg-[#FF7C3A] activity-type absolute top-3 text-white font-semibold text-xs flex items-center pl-3 pr-5 h-7 shadow-sm">
-                    {{ item.types }}
+                    {{ item?.types }}
                 </div>
                 <div
                     class="absolute right-3 top-3 bg-white/90 backdrop-blur-sm text-gray-800 text-xs px-3 h-7 rounded-full shadow-sm flex items-center gap-1.5">
@@ -19,7 +19,7 @@
                             d="M0.5 6.9994V10.3327C0.5 11.1827 1.48917 11.9327 3 12.3844C3.945 12.6677 5.09417 12.8327 6.33333 12.8327C7.5725 12.8327 8.72167 12.6669 9.66667 12.3844C11.1775 11.9327 12.1667 11.1827 12.1667 10.3327V9.4994M0.5 6.9994C0.5 6.0019 1.8625 5.14107 3.83333 4.74023M0.5 6.9994C0.5 7.8494 1.48917 8.5994 3 9.05107C3.945 9.3344 5.09417 9.4994 6.33333 9.4994C6.9125 9.4994 7.47167 9.46357 8 9.39607"
                             stroke="#FF5722" stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
-                    <span class="font-semibold text-xs">{{ item.karma_points?.toLocaleString() }} Points</span>
+                    <span class="font-semibold text-xs">{{ item?.karma_points?.toLocaleString() }} Points</span>
                 </div>
                 <div class="absolute bottom-3 flex items-center gap-2 px-3 overflow-auto">
                     <div v-if="item.sdgs" v-for="el in JSON.parse(item.sdgs)" class="flex-shrink-0">
@@ -84,14 +84,14 @@
     <article v-else class="flex flex-col card w-full py-2 overflow-hidden">
         <router-link :to="dynamicLink" class="block">
             <div class="w-full flex flex-col justify-between aspect-[16/9] rounded-lg relative overflow-hidden">
-                <img :src="item.activity_image || 'https://res.cloudinary.com/dyt5jqnax/image/upload/v1742968038/mykartavya-logo_jptv31.png'"
+                <img :src="item?.activity_image || 'https://res.cloudinary.com/dyt5jqnax/image/upload/v1742968038/mykartavya-logo_jptv31.png'"
                     alt=""
                     class="h-full w-full object-cover rounded-lg transition-transform duration-300 hover:scale-105">
                 <div
                     class="bg-[#FF7C3A] activity-type absolute top-3 text-white font-semibold text-xs flex items-center pl-3 pr-5 h-7 shadow-sm">
                     {{ item.types }}
                 </div>
-                <div v-if="item.karma_points"
+                <div v-if="item?.karma_points"
                     class="absolute right-3 top-3 bg-white/90 backdrop-blur-sm text-gray-800 text-xs px-3 h-7 rounded-full shadow-sm flex items-center gap-1.5">
                     <svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
@@ -101,7 +101,7 @@
                             d="M0.5 6.9994V10.3327C0.5 11.1827 1.48917 11.9327 3 12.3844C3.945 12.6677 5.09417 12.8327 6.33333 12.8327C7.5725 12.8327 8.72167 12.6669 9.66667 12.3844C11.1775 11.9327 12.1667 11.1827 12.1667 10.3327V9.4994M0.5 6.9994C0.5 6.0019 1.8625 5.14107 3.83333 4.74023M0.5 6.9994C0.5 7.8494 1.48917 8.5994 3 9.05107C3.945 9.3344 5.09417 9.4994 6.33333 9.4994C6.9125 9.4994 7.47167 9.46357 8 9.39607"
                             stroke="#FF5722" stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
-                    <span class="font-semibold text-xs">{{ item.karma_points?.toLocaleString() }} Points</span>
+                    <span class="font-semibold text-xs">{{ item?.karma_points?.toLocaleString() }} Points</span>
                 </div>
                 <div class="absolute bottom-3 flex items-center gap-2 px-3 overflow-auto">
                     <div v-if="item.sdgs" v-for="el in JSON.parse(item.sdgs)" class="flex-shrink-0">
@@ -114,10 +114,10 @@
             </div>
             <div class="px-1">
                 <h3 class="self-start truncate mt-3 text-base font-semibold tracking-normal text-gray-900">
-                    {{ item.activity_name }}
+                    {{ item?.activity_name }}
                 </h3>
                 <p class="mt-2 text-sm tracking-normal leading-5 text-justify text-gray-600 line-clamp-2">
-                    {{ item.opportunity_description }}
+                    {{ item?.opportunity_description }}
                 </p>
                 <div class="flex flex-col gap-3 justify-between mt-3">
                     <div class="flex gap-1.5 items-center text-xs tracking-normal text-gray-600">
@@ -131,21 +131,21 @@
                                 d="M12 10.3333C12 10.5101 11.9298 10.6797 11.8047 10.8047C11.6797 10.9298 11.5101 11 11.3333 11C11.1565 11 10.987 10.9298 10.8619 10.8047C10.7369 10.6797 10.6667 10.5101 10.6667 10.3333C10.6667 10.1565 10.7369 9.98695 10.8619 9.86193C10.987 9.7369 11.1565 9.66667 11.3333 9.66667C11.5101 9.66667 11.6797 9.7369 11.8047 9.86193C11.9298 9.98695 12 10.1565 12 10.3333ZM12 7.66667C12 7.84348 11.9298 8.01305 11.8047 8.13807C11.6797 8.2631 11.5101 8.33333 11.3333 8.33333C11.1565 8.33333 10.987 8.2631 10.8619 8.13807C10.7369 8.01305 10.6667 7.84348 10.6667 7.66667C10.6667 7.48986 10.7369 7.32029 10.8619 7.19526C10.987 7.07024 11.1565 7 11.3333 7C11.5101 7 11.6797 7.07024 11.8047 7.19526C11.9298 7.32029 12 7.48986 12 7.66667ZM8.66667 10.3333C8.66667 10.5101 8.59643 10.6797 8.4714 10.8047C8.34638 10.9298 8.17681 11 8 11C7.82319 11 7.65362 10.9298 7.5286 10.8047C7.40357 10.6797 7.33333 10.5101 7.33333 10.3333C7.33333 10.1565 7.40357 9.98695 7.5286 9.86193C7.65362 9.7369 7.82319 9.66667 8 9.66667C8.17681 9.66667 8.34638 9.7369 8.4714 9.86193C8.59643 9.98695 8.66667 10.1565 8.66667 10.3333ZM8.66667 7.66667C8.66667 7.84348 8.59643 8.01305 8.4714 8.13807C8.34638 8.2631 8.17681 8.33333 8 8.33333C7.82319 8.33333 7.65362 8.2631 7.5286 8.13807C7.40357 8.01305 7.33333 7.84348 7.33333 7.66667C7.33333 7.48986 7.40357 7.32029 7.5286 7.19526C7.65362 7.07024 7.82319 7 8 7C8.17681 7 8.34638 7.07024 8.4714 7.19526C8.59643 7.32029 8.66667 7.48986 8.66667 7.66667ZM5.33333 10.3333C5.33333 10.5101 5.2631 10.6797 5.13807 10.8047C5.01305 10.9298 4.84348 11 4.66667 11C4.48986 11 4.32029 10.9298 4.19526 10.8047C4.07024 10.6797 4 10.5101 4 10.3333C4 10.1565 4.07024 9.98695 4.19526 9.86193C4.32029 9.7369 4.48986 9.66667 4.66667 9.66667C4.84348 9.66667 5.01305 9.7369 5.13807 9.86193C5.2631 9.98695 5.33333 10.1565 5.33333 10.3333ZM5.33333 7.66667C5.33333 7.84348 5.2631 8.01305 5.13807 8.13807C5.01305 8.2631 4.84348 8.33333 4.66667 8.33333C4.48986 8.33333 4.32029 8.2631 4.19526 8.13807C4.07024 8.01305 4 7.84348 4 7.66667C4 7.48986 4.07024 7.32029 4.19526 7.19526C4.32029 7.07024 4.48986 7 4.66667 7C4.84348 7 5.01305 7.07024 5.13807 7.19526C5.2631 7.32029 5.33333 7.48986 5.33333 7.66667Z"
                                 fill="currentColor" />
                         </svg>
-                        <time class="self-stretch my-auto font-medium">{{ formatDate(item.start_date) }} - {{
-                            formatDate(item.end_date)
+                        <time class="self-stretch my-auto font-medium">{{ formatDate(item?.start_date) }} - {{
+                            formatDate(item?.end_date)
                         }}</time>
                     </div>
                     <div class="flex gap-2 items-center text-gray-600">
                         <Tooltip text="Hours">
                             <FeatherIcon name="clock" class="size-4" />
-                            <p class="text-xs font-medium">{{ item.hours }} hr</p>
+                            <p class="text-xs font-medium">{{ item?.hours }} hr</p>
                         </Tooltip>
                     </div>
                     <div class="flex items-center justify-between">
                         <div v-if="parseVolunteers(item?.volunteers).length > 0 && parseVolunteers(item?.volunteers)[0]?.full_name"
                             class="flex items-center -space-x-3">
                             <div v-for="(el, index) in parseVolunteers(item?.volunteers).slice(0, 3)" :key="index">
-                                <img v-if="el?.user_image" :src="el.user_image" :alt="'User ' + (index + 1)"
+                                <img v-if="el?.user_image" :src="el?.user_image" :alt="'User ' + (index + 1)"
                                     class="w-8 h-8 rounded-full border-2 border-white" />
                                 <div v-else
                                     class="w-8 h-8 flex items-center justify-center rounded-full border-2 border-[#e86c13] text-sm">
