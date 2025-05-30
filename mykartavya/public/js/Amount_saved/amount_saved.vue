84d1303fdebd598b7fa742b2c19b6cd14b0d784e
Bittu Kumar <bittusuvaidyam@outlook.com>
2025-05-30 18:24:53 +0530

    add amount_saved chart to dashboard

--- mykartavya/public/js/Amount_saved/amount_saved.vue
+++ mykartavya/public/js/Amount_saved/amount_saved.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h3 style="font-size: 16px;font-weight: 400; color:#7c7c7c;">
-            Charts for hours Donated
+            Charts for Amount Saved
         </h3>
         <canvas ref="chartCanvas" width="400" height="150"></canvas>
     </div>
@@ -12,32 +12,32 @@ import { onMounted, ref } from 'vue'
 import { Chart } from 'chart.js/auto'
 
 const chartCanvas = ref(null)
+const sdgColors = {
+  "Clean Water and Sanitation":                "#26BDE2",
+  "Climate Action":                            "#3F7E44",
+  "Partnerships for the Goals":                "#19486A",
+  "Industry Innovation and Infrastructure":    "#FD6925",
+  "Decent Work and Economic Growth":           "#A21942",
+  "Responsible Consumption and Production":    "#BF8B2E",
+  "Peace Justice and Strong Institutions":     "#00689D",
+  "Life on Land":                              "#56C02B",
+  "Gender Equality":                           "#FF3A21",
+  "Life Below Water":                          "#0A97D9",
+  "Sustainable Cities and Communities":        "#FD9D24",
+  "Quality Education":                         "#C5192D",
+  "No Poverty":                                "#E5243B",
+  "Good Health and Well-being":                "#4C9F38",
+  "Affordable and Clean Energy":               "#FCC30B",
+  "Zero Hunger":                               "#DDA63A",
+  "Reduced Inequality":                        "#DD1367"
+}
 
-const sdgColors = [
-    "#26BDE2", // Clean Water and Sanitation (SDG 6)
-    "#3F7E44", // Climate Action (SDG 13)
-    "#19486A", // Partnerships for the Goals (SDG 17)
-    "#FD6925", // Industry Innovation and Infrastructure (SDG 9)
-    "#A21942", // Decent Work and Economic Growth (SDG 8)
-    "#BF8B2E", // Responsible Consumption and Production (SDG 12)
-    "#00689D", // Peace, Justice and Strong Institutions (SDG 16)
-    "#56C02B", // Life on Land (SDG 15)
-    "#FF3A21", // Gender Equality (SDG 5)
-    "#0A97D9", // Life Below Water (SDG 14)
-    "#FD9D24", // Sustainable Cities and Communities (SDG 11)
-    "#C5192D", // Quality Education (SDG 4)
-    "#E5243B", // No Poverty (SDG 1)
-    "#4C9F38", // Good Health and Well-being (SDG 3)
-    "#FCC30B", // Affordable and Clean Energy (SDG 7)
-    "#DDA63A", // Zero Hunger (SDG 2)
-    "#DD1367"  // Reduced Inequality (SDG 10)
-]
 
 onMounted(() => {
     frappe.call({
         method: 'frappe.desk.query_report.run',
         args: {
-            report_name: 'Charts for hours Donated'
+            report_name: 'Chart for Amount Saved'
         },
         callback: (r) => {
             const result = r.message
@@ -46,10 +46,15 @@ onMounted(() => {
                 console.error("No data found in report")
                 return
             }
+            console.log("result", result.result)
 
 
+            // const labels = result.result.map(item => item.sdg_name)
+            // const values = result.result.map(item => item.total_hours)
             const labels = result.result.map(item => item.sdg_name)
-            const values = result.result.map(item => item.total_hours)
+            const values = result.result.map(item => item.work_value_rupees)
+            const backgroundColors = labels.map(label => sdgColors[label] || '#ccc') 
+
 
             if (chartCanvas.value) {
                 new Chart(chartCanvas.value, {
@@ -59,8 +64,8 @@ onMounted(() => {
                         datasets: [{
                             label: result.columns[1].label,
                             data: values,
-                            backgroundColor: sdgColors.slice(0, values.length),
-                            hoverBackgroundColor: sdgColors.slice(0, values.length),
+                            backgroundColor:backgroundColors,
+                            hoverBackgroundColor: backgroundColors,
                             borderColor: 'rgba(153, 102, 255, 1)',
                             borderWidth: 1
                         }]
